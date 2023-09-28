4f6d82b470a77f034ee3ee218e663367835df375
IkbalEddiani <ikbaleddiani@gmail.com>
2023-09-27 18:49:09 -0700

    Manifest to install Nginx web server using Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,34 @@
 # Manifest that configures an Nginx web server using Puppet
 
-exec {'apt_update':
-  command     => 'sudo apt-get update',
+exec { 'apt_update':
+  command     => 'apt-get update',
   path        => '/usr/bin:/usr/sbin',
   refreshonly => true,
 }
 
-package {'nginx':
-  ensure  => 'installed',  
+package { 'nginx':
+  ensure  => 'installed',
   require => Exec['apt_update'],
 }
 
-service {'nginx':
+service { 'nginx':
   ensure  => 'running',
   enable  => true,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
 
-file {'/var/www/html/index.html':
+file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
-  require => package['nginx'],
+  require => Package['nginx'],
 }
 
-file_line {'redirect':
+file_line { 'redirect':
   path    => '/etc/nginx/sites-available/default',
   line    => 'location /redirect_me { return 301 https://www.youtube.com/; }',
   match   => '^\troot /var/www/html;$',
   ensure  => 'present',
-  require => package['nginx'],
+  require => Package['nginx'],
   notify  => Service['nginx'],
 }
+
